6bc2941d2504564b457d996dbe23bdff5fe71ebb
Caio Henrique <caiohenriqueviana01@gmail.com>
2024-04-19 22:23:53 -0300

    acabou a aula do dia 19/04

---
+++ aula-12/app/src/main/java/ads/poo/Autor.java
@@ -0,0 +1,11 @@
+package ads.poo;
+
+public class Autor {
+    private String titulo;
+    private String nacionalidade;
+
+    public Autor(String titulo, String nacionalidade) {
+        this.titulo = titulo;
+        this.nacionalidade = nacionalidade;
+    }
+}
